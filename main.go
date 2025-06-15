1a86ed24927592eb0ce66518a6da4a776221c388
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-06-15 16:53:59 +0900

    飲食店の情報を返す

--- main.go
+++ main.go
@@ -1,33 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/v8/linebot"
 )
 
-func init() {
-	godotenv.Load()
-}
-
 func main() {
+	godotenv.Load()
+
 	// ハンドラの登録
 	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/callback", lineHandler)
 
-	// RenderではPORT環境変数を使う必要がある
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // ローカル用のデフォルト
-	}
-
-	fmt.Printf("Server is running on http://localhost:%s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	fmt.Println("http://localhost:8080 で起動中...")
+	// HTTPサーバを起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,29 +30,18 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func lineHandler(w http.ResponseWriter, r *http.Request) {
-	// 環境変数からLINEチャネル情報を取得
-	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
-	channelToken := os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
-
-	fmt.Println("Secret:", channelSecret)
-	fmt.Println("Token:", channelToken)
-
-	// ログで確認
-	log.Printf("Using channel secret: %s", maskString(channelSecret))
-	log.Printf("Using access token: %s", maskString(channelToken))
-
 	// BOTを初期化
-	bot, err := linebot.New(channelSecret, channelToken)
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"),
+	)
 	if err != nil {
-		log.Printf("Failed to create bot: %v", err)
-		w.WriteHeader(500)
-		return
+		log.Fatal(err)
 	}
 
 	// リクエストからBOTのイベントを取得
 	events, err := bot.ParseRequest(r)
 	if err != nil {
-		log.Printf("Error parsing request: %v", err)
 		if err == linebot.ErrInvalidSignature {
 			w.WriteHeader(400)
 		} else {
@@ -66,50 +49,81 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
-	// イベントごとにログを出力
 	for _, event := range events {
-		log.Printf("Received event: %+v", event)
-
+		// イベントがメッセージの受信だった場合
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
+			// メッセージがテキスト形式の場合
 			case *linebot.TextMessage:
-				log.Printf("Text message: %s", message.Text)
 				replyMessage := message.Text
 				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
 				if err != nil {
-					log.Printf("Failed to reply: %v", err)
+					log.Print(err)
 				}
+			// メッセージが位置情報の場合
 			case *linebot.LocationMessage:
-				log.Printf("Location received: lat=%f, lng=%f", message.Latitude, message.Longitude)
 				sendRestoInfo(bot, event)
-			default:
-				log.Printf("Unhandled message type: %T", message)
 			}
+			// 他にもスタンプや画像、位置情報など色々受信可能
 		}
 	}
 }
 
-// 位置情報を元に緯度・経度を返信する
 func sendRestoInfo(bot *linebot.Client, e *linebot.Event) {
 	msg := e.Message.(*linebot.LocationMessage)
 
-	lat := strconv.FormatFloat(msg.Latitude, 'f', 6, 64)
-	lng := strconv.FormatFloat(msg.Longitude, 'f', 6, 64)
+	lat := strconv.FormatFloat(msg.Latitude, 'f', 2, 64)
+	lng := strconv.FormatFloat(msg.Longitude, 'f', 2, 64)
 
-	replyMsg := fmt.Sprintf("緯度：%s\n経度：%s", lat, lng)
-	log.Printf("Sending reply: %s", replyMsg)
+	replyMsg := getRestoInfo(lat, lng)
 
 	_, err := bot.ReplyMessage(e.ReplyToken, linebot.NewTextMessage(replyMsg)).Do()
 	if err != nil {
-		log.Printf("Failed to send location reply: %v", err)
+		log.Print(err)
 	}
 }
 
-// セキュリティのためにログ出力時に値をマスクする
-func maskString(s string) string {
-	if len(s) <= 6 {
-		return "******"
+// response APIレスポンス
+type response struct {
+	Results results `json:"results"`
+}
+
+// results APIレスポンスの内容
+type results struct {
+	Shop []shop `json:"shop"`
+}
+
+// shop レストラン一覧
+type shop struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+
+func getRestoInfo(lat string, lng string) string {
+	apikey := "(自分のAPIKEYを入力)"
+	url := fmt.Sprintf(
+		"https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?format=json&key=%s&lat=%s&lng=%s",
+		apikey, lat, lng)
+
+	// リクエストしてボディを取得
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return s[:3] + "..." + s[len(s)-3:]
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data response
+	if err := json.Unmarshal(body, &data); err != nil {
+		log.Fatal(err)
+	}
+
+	info := ""
+	for _, shop := range data.Results.Shop {
+		info += shop.Name + "\n" + shop.Address + "\n\n"
+	}
+	return info
 }
