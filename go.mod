2dab93f551fe59a7ba9cfe550e9618c9c76e3b26
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-06-15 15:24:56 +0900

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module restaulant-go-line
+
+go 1.24.4
+
+require github.com/line/line-bot-sdk-go/v8 v8.13.1 // indirect
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/line/line-bot-sdk-go/v8/linebot"
+)
+
+func main() {
+	// ハンドラの登録
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/callback", lineHandler)
+
+	fmt.Println("http://localhost:8080 で起動中...")
+	// HTTPサーバを起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	msg := "Hello World!!!!"
+	fmt.Fprintf(w, msg)
+}
+
+func lineHandler(w http.ResponseWriter, r *http.Request) {
+	// BOTを初期化
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// リクエストからBOTのイベントを取得
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		// イベントがメッセージの受信だった場合
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// メッセージがテキスト形式の場合
+			case *linebot.TextMessage:
+				replyMessage := message.Text
+				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				if err != nil {
+					log.Print(err)
+				}
+			}
+			// 他にもスタンプや画像、位置情報など色々受信可能
+		}
+	}
+}
